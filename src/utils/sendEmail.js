fc0d18dfec66b9dfae6cc6812324e95d8a250afb
Pramodpulicherla-279 <pramodpulicherla279@gmail.com>
2026-06-01 08:01:52 +0530

    fixed some bugs

--- src/utils/sendEmail.js
+++ src/utils/sendEmail.js
@@ -1,11 +1,17 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async ({ to, subject, html }) => {
+  if (!process.env.EMAIL_USER || !process.env.EMAIL_PASS) {
+    throw new Error('Email service is not configured. Set EMAIL_USER and EMAIL_PASS environment variables.');
+  }
+
   const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false, // TLS
     auth: {
       user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      pass: process.env.EMAIL_PASS, // Use a Gmail App Password, not your account password
     },
   });
 
